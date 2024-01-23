36844064db716f75bff528bf80bc8b0d29a90d08
N44zxyy <n44zxyy@gmail.com>
2024-01-23 23:49:34 +0530

    Initial Commit

---
+++ assets/js/commands.js
@@ -0,0 +1,65 @@
+const commands = {
+  "about":{
+    "description":"Introduction",
+    "multiline":false,
+    "content":"\nI am Nazim Mohammed, a recent graduate holding a bachelor’s degree in computer applications. As a dynamic and self-motivated fresher, I have developed proficiency in Python, Django, and full-stack web development, with a specialized focus on data science. My skills extend to AI, machine learning, and deep learning, reflecting a commitment to staying abreast of industry advancements. Equipped with essential developer tools, I am eager to contribute to innovative solutions and embrace new projects.\n\n"
+  },
+  "linkedin":{
+    "description":"Get my LinkedIn Profile",
+    "multiline":false,
+    "content":"\nYou can see my LinkedIn Profile at the following link https://www.linkedin.com/in/nazim-mohd/ \n\n"
+  },
+  "github":{
+    "description":"Get my github profile",
+    "multiline":false,
+    "content":"\nYou can see my GitHub Account at the following link https://github.com/N44zxyy \n\n"
+  },
+  "education":{
+    "description":"Get my education timeline",
+    "multiline":true,
+    "content": [
+      "\nBachelor of Computer Application (2020-2023) with a CGPA of 7.2 at Al-Ameen College, Edathala, affiliated to Mahatma Gandhi University.\n",
+      "\nHigher Secondary in Bio-Maths (2018-2020) with a CGPA of 7.4 at St. Joseph's EMHS, Thrikkakkara, affiliated to Kerala DHSE.\n",
+      "\n10th grade in 2018 with a CGPA of 7.2 at Talent Public School, Aluva, affiliated to CBSE.\n\n"
+    ]    
+  },
+  "resume":{
+    "description":"Download my Resume",
+    "multiline":true
+  },
+  "certificate":{
+    "description":"Achieved Certificates",
+    "multiline":true,
+    "content":[
+      "\nPYTHON AND DJANGO FULL STACK WEB DEVELOPER",
+      "Udemy | 2023",
+      "\nDATA SCIENTIST CERTIFICATION",
+      "ExpertzLab | Affiliated to National Council for Technology and Training | 2024\n\n"
+    ]
+  },
+  "python":{
+    "description":"View my Certificate",
+    "multiline":true
+  },
+  "project":{
+    "description":"Some projects I've created.",
+    "multiline":true,
+    "content":[
+      "\nMUSEX DATING APP (FULL-STACK WEB DEVELOPMENT)\n",
+      "Role: Python Full-Stack Developer | Technologies: Python, Django, JavaScript, HTML, CSS, SQL",
+      "Achievements:",
+      "➢  Developed matchmaking algorithms for mutual music preferences.",
+      "➢  Designed an intuitive user interface for seamless user experience.",
+      "➢  Integrated a music recommendation API for enhanced matching accuracy.\n",
+    
+      "\nINFUEL - FUEL DELIVERY MANAGEMENT SYSTEM (PHP AND MYSQL)\n",
+      "Role: Full-Stack Developer | Technologies: PHP, MySQL, JavaScript, HTML, CSS.",
+      "Achievements:",
+      "➢  Implemented order processing, delivery tracking, and customer management features.",
+      "➢  Optimized the database schema for efficient data storage and retrieval.\n\n",
+
+      "AND MANY MORE !\n\n"
+    ]
+  }
+}
+
---
+++ assets/js/index.js
@@ -0,0 +1,157 @@
+function updateScroll(){
+  let element = document.getElementById("screen");
+  element.scrollTop = element.scrollHeight;
+}
+
+function generateLine(text,html){
+  let line = document.createElement("div");
+  line.className = "line";
+  if(html){
+    line.innerHTML = text;
+  }else{
+    line.innerText = text;
+  }
+ 
+  return line;
+}
+
+function insertLine(text,html =false){
+  let newLine = generateLine(text,html);
+  $("#screen").append(newLine);
+  updateScroll();
+  return newLine;
+}
+
+function loading(){
+  console.log("Loading");
+  let insertedLine = insertLine("$ Loading console");
+  let counter = 0;
+
+  loadingMessage = () =>{
+    if(counter == 5){
+      clearInterval(interval);
+      $('#input').prop('disabled',false);
+      insertLine("\n");
+      insertLine(
+        "███░░░░██░░█████░░███████░██░░░░███████░██░░░██░\n" +
+        "████░░░██░██░░░██░░░░███░░██░░░░██░░░░░░██░░░██░\n" +
+        "██░██░░██░███████░░░███░░░██░░░░███████░███████░\n" +
+        "██░░██░██░██░░░██░░███░░░░██░░░░░░░░░██░██░░░██░\n" +
+        "██░░░████░██░░░██░███████░██░██░███████░██░░░██░\n" );
+      insertLine("\n");
+      insertLine("To see the list of commands type '<strong>help</strong>' or type '<strong>cv</strong>' to see all the information",true);
+      $('#input').focus();
+      return;
+    }
+    insertedLine.innerText += ".";
+    counter+=1
+  }
+  interval = setInterval(loadingMessage,350);
+
+}
+
+function readCommand(event){
+  if (event.key != "Enter") {
+    return;
+  }
+  const command = event.target.value.trim().toLowerCase();
+  event.target.value='';
+
+  if(command === ""){
+    return;
+  }
+
+  if(command == "help"){
+    helpCommand();
+    return;
+  }
+
+  if(command == "cv"){
+    cvCommand();
+    return;
+  }
+
+  if(command == "github"){
+    window.open('https://github.com/N44zxyy','_blank');
+    return;
+  }
+
+  if(command == "linkedin"){
+    window.open('https://www.linkedin.com/in/nazim-mohd','_blank');
+    return;
+  }
+
+  if(command == "python"){
+    window.open('https://www.udemy.com/certificate/UC-e45452e0-e5bb-402e-9ef5-8d00ea4b2c3a/','_blank');
+    return;
+  }
+
+  if(command == "resume"){
+    window.open('./assets/nazim-resume.pdf','_blank');
+    return;
+  }
+
+  if(command == "cls"){
+    clearCommand();
+    return;
+  }
+
+  let element = commands[command];
+
+  if(element){
+    printContent(element,"$ ");
+    return;
+  }
+
+  insertLine(`Command '${command}' is not recognized as an internal command.`);
+}
+
+
+function helpCommand(){
+  insertLine("\nList all command information");
+  insertLine("\n");
+  insertLine("<strong>cv</strong>: Show all CV information",true);
+  let commandKeys = Object.keys(commands);
+  for(let i = 0; i < commandKeys.length; i++){
+    let command = commands[commandKeys[i]];
+    insertLine(`<strong>${commandKeys[i]}</strong>: ${command.description}`,true);
+  }
+  insertLine("\n");
+  insertLine("<strong>cls</strong>: Clear the console",true);
+}
+
+function cvCommand(){
+  insertLine("\n");
+  insertLine("\n$ <strong>--Information--</strong>\n",true);
+  printContent(commands['about']);
+  insertLine("\n<strong>$ --Education--</strong>\n",true);
+  printContent(commands['education']);
+  insertLine("\n<strong>$ --Project--</strong>\n",true);
+  printContent(commands['projects']);
+  insertLine("\n<strong>$ --certificate--</strong>\n",true);
+  printContent(commands['certificate']);
+  insertLine("\n<strong>$ --LinkedIn--</strong>\n",true);
+  printContent(commands['linkedin']);
+  insertLine("\n<strong>$ --GitHub--</strong>\n",true);
+  printContent(commands['github']);
+}
+
+function clearCommand(){
+  let element = document.getElementById("screen");
+  element.innerHTML = "";
+  updateScroll();
+}
+
+function printContent(command,prefix=""){
+  if(command.multiline){
+    if(prefix != ''){
+      insertLine(prefix)
+    }
+    for(let x = 0; x< command.content.length; x++){
+      insertLine(`${command.content[x]}`)
+    }
+    return;
+  }
+  insertLine(`${prefix}${command.content}`)
+
+}
